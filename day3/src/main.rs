d736d3794a91ce394c764eb4589f6dc1e5154ee1
Keiran Smith <contact@keiran.scot>
2023-12-05 09:51:41 +0000

    Day 3 Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -43,6 +43,38 @@ fn is_near_symbol(map: &[&str], num_str: &str, start_x: usize, y: usize) -> bool
     false
 }
 
+fn find_symbol(lines: &[&[u8]], r: usize, c: usize) -> Option<(usize, usize, char)> {
+  for (dr, dc) in [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)] {
+    let (rr, cc) = ((r as i32 + dr) as usize, (c as i32 + dc) as usize);
+    let Some(&s) = lines.get(rr).and_then(|l| l.get(cc)) else {continue};
+    if s != b'.' && !s.is_ascii_digit() {
+      return Some((cc, rr, s as char));
+    }
+  }
+  None
+}
+
+use std::collections::HashMap;
+
+fn get_symbols(lines: &[&[u8]]) -> HashMap<(usize, usize, char), Vec<usize>> {
+    let mut symbols = HashMap::new();
+    for (r, l) in lines.iter().enumerate() {
+        let mut c = 0;
+        while c < l.len() {
+        let (start, mut symbol) = (c, None);
+        while c < l.len() && l[c].is_ascii_digit() {
+            symbol = symbol.or_else(|| find_symbol(&lines, r, c));
+            c += 1;
+        }
+        if let Some(symbol) = symbol {
+            let num = l[start..c].iter().fold(0, |n, c| n * 10 + (c - b'0') as usize);
+            symbols.entry(symbol).or_insert(Vec::new()).push(num);
+        }
+        c += 1;
+        }
+    }
+    symbols
+}
 
 fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
 where
@@ -64,6 +96,20 @@ fn main() {
         }
     }
     println!("Day 3 Part 1 : {}", result);
+
+
+    let lines = include_str!("../input");
+    let lines = lines.split("\n").map(str::as_bytes).collect::<Vec<_>>();
+
+    let symbols = get_symbols(&lines);
+
+    let p2: usize = symbols.iter()
+        .filter(|(&(_, _, s), v)| s == '*' && v.len() == 2)
+        .map(|(_, v)| v[0] * v[1])
+        .sum();
+    
+    println!("Day 3 Part 2 : {}", p2);
+
 }
 
 #[cfg(test)]
